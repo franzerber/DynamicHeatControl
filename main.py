754195a2dfe39da6e44509adaef0e895d819dd5f
Franz Erber <franz.erber@aon.at>
2024-11-29 19:12:51 +0000

    change telegram message

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
-import requests
-from datetime import datetime, timedelta
-from influxdb import InfluxDBClient
 import json
+from datetime import datetime, timedelta
+from typing import List, Dict, Union
+from influxdb import InfluxDBClient
+import requests
 
 SHUTDOWN_PRICE = 15 # Cent/kWh
 MINIMUM_FREE_PV_POWER = 2500 # W    
-INTERVAL = 30 # minutes
+INTERVAL = 15 # minutes
 
-with open("settings.json", "r") as f:
+with open("settings.json", "r", encoding="utf-8") as f:
     settings = json.load(f)
     INFLUX_USER = settings["influx_user"]
     INFLUX_PASSWORD = settings["influx_password"]
+    INFLUX_HOST = settings["influx_host"]
+    TELEGRAM_TOKEN = settings["telegram_token"]
+    TELEGRAM_CHAT_ID = settings["telegram_chat_id"]
 
 def get_current_pv_power(measurement, fieldname) -> int:
     """
     query the free PV power from influxdb
     """
-    client = InfluxDBClient(host='10.88.88.1', port=8086, username=INFLUX_USER, 
-                            password=INFLUX_PASSWORD, database='evn_db_token')
+    client = InfluxDBClient(host='10.88.88.1', port=8086,username=INFLUX_USER,password=INFLUX_PASSWORD, database='evn_db_token')
     query = f'SELECT last("{fieldname}") FROM "{measurement}" WHERE time > now() - 1h'
     result = client.query(query)
     points = list(result.get_points())
@@ -26,38 +29,60 @@ def get_current_pv_power(measurement, fieldname) -> int:
     else:
         return 0
 
-
-def get_awattar_prices() -> list:
+def get_awattar_prices() -> List[Dict]:
     """
     query awattar for the next available prices, returns a list of dictionaries
     with the following keys: 'start_time', 'end_time', 'price'
     """
     url = "https://api.awattar.de/v1/marketdata"
-    response = requests.get(url)
+    response = requests.get(url, timeout=10)
     if response.status_code == 200:
         data = response.json()
-        prices = []
+        current_prices = []
         for entry in data['data']:
             start_time = datetime.fromtimestamp(entry['start_timestamp'] / 1000)
             end_time = datetime.fromtimestamp(entry['end_timestamp'] / 1000)
             price = round(entry['marketprice'] / 10, 2)  # Convert from €/MWh to €/kWh and round to 2 decimal places
-            prices.append({
+            current_prices.append({
                 'start_time': start_time,
                 'end_time': end_time,
                 'price': price
             })
-        return prices
+        return current_prices
     else:
-        response.raise_for_status()
+        return []
+
+def send_telegram_message(token: str, chat_id: str, message: str) -> None:
+    """
+    Send a message to a Telegram bot.
+    """
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    payload = {
+        'chat_id': chat_id,
+        'text': message
+    }
+    response = requests.post(url, json=payload, timeout=10)
+    if response.status_code != 200:
+        raise requests.exceptions.RequestException(f"Failed to send message: {response.text}")
+
 
 # Example usage
 if __name__ == "__main__":
     prices = get_awattar_prices()
+    PRICE_TABLE = ""
     for price in prices:
         print(f"From {price['start_time']} to {price['end_time']}: {price['price']} €/kWh")
+        PRICE_TABLE += (
+            f"{price['start_time'].strftime('%y-%m-%d %H')}:00 - "
+            f"{price['end_time'].strftime('%H:%M')}: {price['price']} €/kWh\n"
+        )
     power_consumtion = get_current_pv_power('evn_bg3_1_token','MomentanleistungP')
     print(f"BG3 current power consumtion: {power_consumtion} W")
     free_power = get_current_pv_power('evn_bg1_token','MomentanleistungN')
     print(f"BG1 current free power: {free_power} W")
     free_energy = free_power - power_consumtion
     print(f"current free energy: {free_energy} W")
+    send_telegram_message(TELEGRAM_TOKEN, TELEGRAM_CHAT_ID,
+                          f"BG3 current power consumtion: {power_consumtion} W\n"
+                          f"BG1 current free power: {free_power} W\n"
+                          f"current free energy: {free_energy} W\n{PRICE_TABLE}")
